e3e4e9b54bb286b279b80ed3b3658ec06febcbe8
github username <github email address>
2021-06-15 20:47:28 -0400

    migrates to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_05_04_180515) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "games", force: :cascade do |t|
     t.integer "card_number"
     t.integer "click_number", default: 0
